5e1d20be0b68fe2a208af893d07431db714bdbbb
Anas-tec <a.anas.muhammed25@gmail.com>
2025-05-28 16:36:33 +0530

    Some changes are made

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -4,6 +4,7 @@ namespace CrudTest.Models
 {
     public class AppDbContext:DbContext
     {
+        //public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
