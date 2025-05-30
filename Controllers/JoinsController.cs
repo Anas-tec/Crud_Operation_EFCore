3f3dc23c26c3611ea77c7800b326ca5bd064477f
Anas-tec <a.anas.muhammed25@gmail.com>
2025-05-30 16:01:13 +0530

    <>|Muhammed anas|<Selection operation on Country and State>

---
+++ Controllers/JoinsController.cs
@@ -0,0 +1,38 @@
+﻿using CrudTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudTest.Controllers
+{
+    public class JoinsController : Controller
+    {
+
+        private readonly AppDbContext _context;
+         public JoinsController(AppDbContext context)
+         {
+             _context = context;
+         }
+
+         public async Task<IActionResult> Index()
+         {
+            var countries = await _context.Countries.ToListAsync();
+            ViewBag.Countries = countries; 
+            return View(countries);
+         }
+         [HttpGet]
+         public async Task<JsonResult> GetStates(int countryId)
+         {
+             var states = await _context.States
+                 .Where(s => s.CountryId == countryId)
+                 .ToListAsync();
+             return Json(states);
+         }
+        
+       // public IActionResult Index() { return View(); }
+
+    }
+
+}
+
+
---
+++ Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudTest.Models
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<State> States { get; set; }
+    }
+}
