c9d5f2a42d96dc3be5ef0c4b2daabd38c1f18980
Anas-tec <a.anas.muhammed25@gmail.com>
2025-06-02 13:31:04 +0530

    <>|Country and state in table|<Anas>

---
+++ Models/CountryStateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CrudTest.Models
+{
+    public class CountryStateViewModel
+    {
+        public string CountryName { get; set; }
+        public string StateName { get; set; }
+    }
+}
